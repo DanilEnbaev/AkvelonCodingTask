56537896b550737a6bbfbf61b807bb78bce29f25
Danil IenbaIev <d.enbaev2003@gmail.com>
2021-11-18 12:37:13 +0200

    Task has done

--- src/main/java/InvalidInputForAnagram.java
+++ src/main/java/InvalidInputForAnagram.java
@@ -1,3 +1,11 @@
+/**
+ * <h1>Class InvalidInputForAnagram </h1>
+ * This class extends Exception and make a new exception for invalid input
+ *
+ * @author Danil Enbaev
+ * @version 1.0
+ * @since 18.11.21
+ */
 public class InvalidInputForAnagram extends Exception {
     String messageOfException;
 
